ab471d04c566a3e99348b153600497773a110e00
olutobikareem <kareemolutobi@gmail.com>
2024-10-06 18:05:26 +0700

    feat: individual products

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -14,10 +14,13 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const router = useRouter();
   const handleClick = () => {
-
-  }
+    router.push(`product/${data?.id}`);
+  };
   return (
-    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4 ">
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4 "
+    >
       {}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
         <Image
